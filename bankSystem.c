c1d3219b5f8fbed1f62b0124e79426177f73c0ea
borisGansGit <bgans.ieu2023@student.ie.edu>
2024-04-21 15:50:06 +0200

    just working on program

--- bankSystem.c
+++ bankSystem.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <sqlite3.h>
+// #include <sqlite3.h>
 
 
 #define MAX_CHAR 100 
@@ -21,34 +21,7 @@ int main() {
     printf("Welcome to BB (Boris' Bank)\n");
     bool cont = true;
 
-    sqlite3 *db;
-    int rc;
-    rc = sqlite3_open("data/lamp.db", &db);
-
-    sqlite3_stmt *stmt;
-    const char *sql = "SELECT * FROM BANK_ACCOUNTS"; // Replace "your_table" with the name of your table
-
-    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "Failed to prepare query: %s\n", sqlite3_errmsg(db));
-        sqlite3_close(db);
-        return 1;
-    }
-
-    // Execute the query and fetch results
-    while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
-        // Process each row of the result set
-        // Example: Retrieve data from columns
-        int id = sqlite3_column_int(stmt, 0); // Assuming the first column is an integer
-        const unsigned char *name = sqlite3_column_text(stmt, 1); // Assuming the second column is a text
-
-        // Process the retrieved data
-        printf("ID: %d, Name: %s\n", id, name);
-    }
-
-    // Finalize the statement and close the database connection
-    sqlite3_finalize(stmt);
-    sqlite3_close(db);
+    
     // here
 
 
